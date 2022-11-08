a5de59fcd95fe5236a28e1e48285fd21668e9e2d
Rebeca Gonzalez <bequipedia@gmail.com>
2022-11-08 15:31:03 +0100

    commented PUT endpoint in borrowed book controller

--- src/main/java/libraryapigit/libraryapigit/controllers/BorrowedBookController.java
+++ src/main/java/libraryapigit/libraryapigit/controllers/BorrowedBookController.java
@@ -44,19 +44,19 @@ public class BorrowedBookController {
   }
 
   // user and book should be modified from their controllers. Might delete later
-  @RequestMapping(value = "/borrowed-books/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-  public @ResponseBody Optional<BorrowedBook> updateBorrowedBookById(@PathVariable String id, @RequestBody BorrowedBook newBorrowedBookDetails) {
-
-    long borrowedBookId = Long.parseLong(id);
-
-    return borrowedBookRepository.findById(borrowedBookId).map(borrowedBook -> {
-
-      borrowedBook.setUser(newBorrowedBookDetails.getUser());
-      borrowedBook.setBook(newBorrowedBookDetails.getBook());
-
-      return borrowedBookRepository.save(borrowedBook);
-    });
-  }
+//  @RequestMapping(value = "/borrowed-books/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+//  public @ResponseBody Optional<BorrowedBook> updateBorrowedBookById(@PathVariable String id, @RequestBody BorrowedBook newBorrowedBookDetails) {
+//
+//    long borrowedBookId = Long.parseLong(id);
+//
+//    return borrowedBookRepository.findById(borrowedBookId).map(borrowedBook -> {
+//
+//      borrowedBook.setUser(newBorrowedBookDetails.getUser());
+//      borrowedBook.setBook(newBorrowedBookDetails.getBook());
+//
+//      return borrowedBookRepository.save(borrowedBook);
+//    });
+//  }
 
   @RequestMapping(value = "/borrowed-books/{id}", method = RequestMethod.DELETE)
   public void deleteBorrowedBookById(@PathVariable String id) {
